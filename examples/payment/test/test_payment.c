cf4f1233cbc4b1844ec0d65ac274d298b22bf4c9
Alvin Beskow <alvinbeskow@hotmail.com>
2021-05-23 16:00:20 -0300

    t2-fix

--- examples/payment/test/test_payment.c
+++ examples/payment/test/test_payment.c
@@ -2,24 +2,66 @@
 #include "unity.h"
 #include "unity_fixture.h"
 
+// 0 - Sucesso; 1 - Erro no valor; 2 - Status incorreto
+
 int expected = 0;
-static char status_str[] = {"regular"};
+static char status_str0[] = {"regular"};
+static char status_str1[] = {"estudante"};
+static char status_str2[] = {"aposentado"};
+static char status_str3[] = {"VIP"};
 
 void expect(int ret_value) {
     expected = ret_value;
 }
 
-void valid(void) {
+void validSt0(void) {
     int ret = 3;
-
-    ret = payment(100.0, status_str);
+    ret = payment(100.0, status_str0);
     TEST_ASSERT_EQUAL_INT(expected, ret);
 }
 
-void invalidValue(void) {
+void validSt1(void) {
+    int ret = 3;
+    ret = payment(100.0, status_str1);
+    TEST_ASSERT_EQUAL_INT(expected, ret);
+}
+
+void validSt2(void) {
+    int ret = 3;
+	expected = 0; 
+    ret = payment(100.0, status_str2);
+    TEST_ASSERT_EQUAL_INT(expected, ret);
+}
+
+void validSt3(void) {
+    int ret = 3;
+    ret = payment(100.0, status_str3);
+    TEST_ASSERT_EQUAL_INT(expected, ret);
+}
+
+void validVal0(void) {
+    int ret = 3;
+    ret = payment(0.02, status_str0);
+    TEST_ASSERT_EQUAL_INT(expected, ret);
+}
+
+void validVal1(void) {
+    int ret = 3;
+    ret = payment(99999.0, status_str0);
+    TEST_ASSERT_EQUAL_INT(expected, ret);
+}
+
+void invalidValue0(void) {
     int ret = 3;
     
-    ret = payment(0.005, status_str);
+    ret = payment(0.005, status_str0);
+    TEST_ASSERT_EQUAL_INT(expected, ret);
+}
+
+void invalidValue1(void) {
+    int ret = 3;
+    
+    ret = payment(99999.99, status_str0);
     TEST_ASSERT_EQUAL_INT(expected, ret);
 }
 
@@ -40,14 +82,44 @@ TEST_SETUP(payment) {
 TEST_TEAR_DOWN(payment) {
 }
 
-TEST(payment, TestValid) {
+TEST(payment, TestValidSt0) {
     expect(0);
-    valid();
+    validSt0();
 }
 
-TEST(payment, TestInvalidValue) {
+TEST(payment, TestValidSt1) {
+    expect(0);
+    validSt1();
+}
+
+TEST(payment, TestValidSt2) {
+    expect(0);
+    validSt2();
+}
+
+TEST(payment, TestValidSt3) {
+    expect(0);
+    validSt3();
+}
+
+TEST(payment, TestValidVal0) {
+    expect(0);
+    validVal0();
+}
+
+TEST(payment, TestValidVal1) {
+    expect(0);
+    validVal1();
+}
+
+TEST(payment, TestInvalidValue0) {
     expect(1);
-    invalidValue();
+    invalidValue0();
+}
+
+TEST(payment, TestInvalidValue1) {
+    expect(1);
+    invalidValue1();
 }
 
 TEST(payment, TestInvalidStatus) {
--- examples/payment/test/test_runners/test_payment_runner.c
+++ examples/payment/test/test_runners/test_payment_runner.c
@@ -2,7 +2,13 @@
 #include "unity_fixture.h"
 
 TEST_GROUP_RUNNER(payment) {
-  RUN_TEST_CASE(payment, TestValid);
-  RUN_TEST_CASE(payment, TestInvalidValue);
+  RUN_TEST_CASE(payment, TestValidSt0);
+  RUN_TEST_CASE(payment, TestValidSt1);
+  RUN_TEST_CASE(payment, TestValidSt2);
+  RUN_TEST_CASE(payment, TestValidSt3);
+  RUN_TEST_CASE(payment, TestValidVal0);
+  RUN_TEST_CASE(payment, TestValidVal1);
+  RUN_TEST_CASE(payment, TestInvalidValue0);
+  RUN_TEST_CASE(payment, TestInvalidValue1);
   RUN_TEST_CASE(payment, TestInvalidStatus);
 }
