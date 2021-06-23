e3e1bcdd2446d0c853c0a4b772b417da99d9941a
Hendric Bataglin Goncalves <hendrick.goncalves@acad.pucrs.br>
2021-06-22 21:49:49 -0300

    adding tests

--- examples/crypt/test/test_crypt.c
+++ examples/crypt/test/test_crypt.c
@@ -10,32 +10,45 @@ static uint32_t input[4];
 enc_dec_type enc_dec = 2;
 enc_type crypt_type;
 
+uint8_t size_test = 0;
+
+uint32_t key_test[9];
+
 /* ----------------------------------------------------------------------------------- */
 
 void initContext(enc_type type, uint32_t *msg, enc_dec_type action) {
     int i;
 
+    memset(key, 0, sizeof(key));
+
     switch (type) {
     case XTEA:
         memcpy(key, KEY_128, sizeof(KEY_128));
+        size_test = 4;
         break;
     case AES128:
         memcpy(key, AES128_KEY_128, sizeof(AES128_KEY_128));
+        size_test = 4;
         break;
     case AES192:
         memcpy(key, AES128_KEY_192, sizeof(AES128_KEY_192));
+        size_test = 6;
         break;
     case AES256:
         memcpy(key, AES128_KEY_256, sizeof(AES128_KEY_256));
+        size_test = 8;
         break;
     case BLOWFISH128:
         memcpy(key, KEY_128, sizeof(KEY_128));
+        size_test = 4;
         break;
     case BLOWFISH192:
         memcpy(key, KEY_192, sizeof(KEY_192));
+        size_test = 6;
         break;
     case BLOWFISH256:
         memcpy(key, KEY_256, sizeof(KEY_256));
+        size_test = 8;
         break;
     default:
         break;
@@ -57,21 +70,11 @@ void CryptLogic(void) {
 
     crypt(key, input, crypt_type, enc_dec, output);
 
-    // printf("\nOutput Encrypted: \n");
-    // for (i = 0; i < 4; i++)
-    //     printf("%08X ", output[i]);
-    // printf("\n");
-    
     enc_dec ^= 1;
 
     crypt(key, output, crypt_type, enc_dec, tmp);
 
     TEST_ASSERT_EQUAL_UINT32_ARRAY(expect, tmp, 4);
-
-    // printf("\nOutput Decrypted: \n");
-    // for (i = 0; i < 4; i++)
-    //     printf("%08X ", tmp[i]);
-    // printf("\n");
 }
 
 void AES128_EncTestAction(enc_type type) {
@@ -95,6 +98,41 @@ void AES128_EncTestAction(enc_type type) {
     
 }
 
+
+void Crypt_KeySizeTestAction(void) { 
+    uint32_t output_golden[4], output[4];
+    int i;
+
+    memset(key_test, 0xFFFFFFFF, sizeof(key_test));
+    memcpy(key_test, key, size_test*sizeof(uint32_t));
+
+    crypt(key, input, crypt_type, enc_dec, output_golden);
+    crypt(key_test, input, crypt_type, enc_dec, output);
+
+    TEST_ASSERT_EQUAL_UINT32_ARRAY(output_golden, output, 4);
+}
+
+void Crypt_InputSizeTestAction(uint32_t *plan_test) {
+    uint32_t output_golden[4], output[4];
+
+    crypt(key, input, crypt_type, enc_dec, output_golden);
+    crypt(key, plan_test, crypt_type, enc_dec, output);
+
+    TEST_ASSERT_EQUAL_UINT32_ARRAY(output_golden, output, 4);
+}
+
+void Crypt_OutputSizeTest(void) {
+    uint32_t output_golden[4], output[5];
+    uint32_t test_var = 0xFFFFFFFF;
+
+    memset(output, 0xFFFFFFFF, sizeof(output));
+
+    crypt(key, input, crypt_type, enc_dec, output_golden);
+    crypt(key, input, crypt_type, enc_dec, output);
+
+    TEST_ASSERT_EQUAL_UINT32 (test_var, output[4]);
+}
+
 TEST_GROUP(crypt);
 
 TEST_SETUP(crypt) {
@@ -116,10 +154,7 @@ TEST(crypt, CryptLogic) {
 
 TEST(crypt, AES128_EncTest) {
     uint8_t i;
-    uint32_t key[8];
-
-    memset(key, 0, sizeof(key));
-
+    
     for (i = 1; i < 4; i++) {
         initContext(i, AES128_PLAN, ENCODER);
         AES128_EncTestAction(i);
@@ -127,3 +162,36 @@ TEST(crypt, AES128_EncTest) {
     
 }
 
+TEST(crypt, Crypt_KeySupSizeTest) { //limite superior
+    uint8_t i;
+    uint32_t plan[4] = {0x01234567, 0x89ABCDEF, 0x01234567, 0x89ABCDEF};
+
+    for (i = 1; i < 7; i++) {
+        initContext(i, plan, ENCODER);
+        Crypt_KeySizeTestAction();
+    }
+    
+}
+
+TEST(crypt, Crypt_InputSizeTest) {
+    uint8_t i;
+    uint32_t plan_test[5] = {0x01234567, 0x89ABCDEF, 0x01234567, 0x89ABCDEF, 0xFFFFFFFF};
+    uint32_t plan_golden[4] = {0x01234567, 0x89ABCDEF, 0x01234567, 0x89ABCDEF};
+
+    for (i = 1; i < 7; i++) {
+        initContext(i, plan_golden, ENCODER);
+        Crypt_InputSizeTestAction(plan_test);
+    }
+    
+}
+
+TEST(crypt, Crypt_OutputSizeTest) {
+    uint8_t i;
+    uint32_t plan[4] = {0x01234567, 0x89ABCDEF, 0x01234567, 0x89ABCDEF};
+
+    for (i = 1; i < 7; i++) {
+        initContext(i, plan, ENCODER);
+        Crypt_OutputSizeTest();
+    }
+    
+}
--- examples/crypt/test/test_runners/test_crypt_runner.c
+++ examples/crypt/test/test_runners/test_crypt_runner.c
@@ -4,6 +4,7 @@
 TEST_GROUP_RUNNER(crypt) {
   RUN_TEST_CASE(crypt, CryptLogic);
   RUN_TEST_CASE(crypt, AES128_EncTest);
-  // RUN_TEST_CASE(crypt, AES192_EncLogic);
-  // RUN_TEST_CASE(crypt, AES256_EncLogic);
+  RUN_TEST_CASE(crypt, Crypt_KeySupSizeTest);
+  RUN_TEST_CASE(crypt, Crypt_InputSizeTest);
+  RUN_TEST_CASE(crypt, Crypt_OutputSizeTest);
 }
